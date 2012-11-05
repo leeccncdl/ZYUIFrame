89880d246ca8ef44a9b7bac8cb5e6c9027a4a3b2
leeccncdl <173257740@qq.com>
2012-11-05 17:33:41 +0800

    常规修改
    二级菜单的滑动效果基本完成，还需完善封装。

--- src/cn/zytec/lee/zyuiframe/MainFrameActivity.java
+++ src/cn/zytec/lee/zyuiframe/MainFrameActivity.java
@@ -9,99 +9,85 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-//import android.widget.FrameLayout;
 import android.widget.ImageView;
 import cn.zytec.lee.app.App;
 
-public class MainFrameActivity extends FragmentActivity implements OnClickListener{
-	
+public class MainFrameActivity extends FragmentActivity implements
+		OnClickListener {
+
+	// about animation
+	private boolean isDrag;
+	private int animationDuration = 250;
+	private Interpolator decelerateInterpolater;
+	private FixedPositionAnimation fixedAnimation;
+	private boolean isLeftPosition = false;
+	private GestureDetector gestureDetector;
+
+	private static int fragmentMovePercent = 10;
+	private int fragmentMoveDistance;
+	private int widthOf60;
+
+	// test
 	private ImageView mic;
 	private Button b1;
 	private Button b2;
-	
-	SecondaryMenuFragment secondaryMenuFrag;
-	
-	private GestureDetector gestureDetector;
-	
-//	private FlingFrameLayout flingFrameLayout;
-	private FrameLayout secondMenuFrameLayout;
-//	private FrameLayout detailFrameLayout;
-
 	private int topMenuPosition = 0;
-	
+
+	// layout and fragment
+	private SecondaryMenuFragment secondaryMenuFrag;
+	private FrameLayout secondMenuFrameLayout;
+
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// TODO Auto-generated method stub
-		return gestureDetector.onTouchEvent(event);  
-	}
+	public void onCreate(Bundle savedInstanceState) {
 
-	
-	public void OpenDetailFragment(int position) {
-		DetailFragment detailFrag = DetailFragment.newInstance(position);
+		super.onCreate(savedInstanceState);
 
-        // Execute a transaction, replacing any existing fragment
-        // with this one inside the frame.
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.detail_view_fl, detailFrag);
-        ft.setTransition(FragmentTransaction.TRANSIT_ENTER_MASK);
-        ft.commit();
-	}
-	
-	public void ClosedDetailFragment(DetailFragment detailFrag) {
-
-
-        // Execute a transaction, replacing any existing fragment
-        // with this one inside the frame.
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.remove(detailFrag);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
-	}
-	
-//	@Override
-//	public View onCreateView(String name, Context context, AttributeSet attrs) {
-//		return super.onCreateView(name, context, attrs);
-//	}
-	
-//	@Override
-//	public boolean onTouchEvent(MotionEvent event) {
-//		return mGallery.onGalleryTouchEvent(event);
-//	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-        super.onCreate(savedInstanceState);
 		DisplayMetrics displayMetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 		App.displayWidth = displayMetrics.widthPixels;
 		App.displayHeight = displayMetrics.heightPixels;
-        
-        setContentView(R.layout.main_frame);
-        
-        gestureDetector = new GestureDetector(this, new GestureListener());
-        
-        showSecondaryMenu(1);
-        findView();
-        addListener();
-        
-    }
-    
-    @Override
-    public void onClick(View v) {
-    	// TODO Auto-generated method stub
-    	switch (v.getId()) {
+		fragmentMoveDistance = CaculateFrameLayoutValue(fragmentMovePercent);
+		widthOf60 = CaculateFrameLayoutValue(60);
+		setContentView(R.layout.main_frame);
+
+		fixedAnimation = new FixedPositionAnimation();
+		gestureDetector = new GestureDetector(this, new GestureListener());
+		decelerateInterpolater = AnimationUtils.loadInterpolator(this,
+				android.R.anim.decelerate_interpolator);
+
+		showSecondaryMenu(0);
+		findView();
+		addListener();
+
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		boolean temp = gestureDetector.onTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_UP && isDrag) {
+			processScrollSnap();
+		}
+		return temp;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
 		case R.id.top_menu_button1:
-			if(topMenuPosition != 0) {
+			if (topMenuPosition != 0) {
 				topMenuPosition = 0;
 				showSecondaryMenu(0);
 			}
 			break;
 		case R.id.top_menu_button2:
-			if(topMenuPosition != 1) {
+			if (topMenuPosition != 1) {
 				topMenuPosition = 1;
 				showSecondaryMenu(1);
 			}
@@ -109,66 +95,176 @@ public class MainFrameActivity extends FragmentActivity implements OnClickListen
 		case R.id.mic:
 			System.out.println("MIC----------------");
 			break;
-			
+
 		default:
 			break;
 		}
-    }
-    
-    private void showSecondaryMenu(int index) {
-    	secondaryMenuFrag = SecondaryMenuFragment.newInstance(index);
+	}
 
-        // Execute a transaction, replacing any existing fragment
-        // with this one inside the frame.
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.secondary_menu_view_fl, secondaryMenuFrag);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
-    }
-    
-    private void findView() {
-    	mic = (ImageView) findViewById(R.id.mic);
-    	b1 = (Button) findViewById(R.id.top_menu_button1);
-    	b2 = (Button) findViewById(R.id.top_menu_button2);
-    	secondMenuFrameLayout = (FrameLayout) findViewById(R.id.secondary_menu_view_fl);
+	/**
+	 * @description 详细页面的fragment在最上层的FrameLayout上，该方法由SecondaryFragment从Menu中触发
+	 * @param position secondaryMenu（fragment）的所选菜单Position
+	 */
+	public void OpenDetailFragment(int position) {
+		DetailFragment detailFrag = DetailFragment.newInstance(position);
 
-    }
-    
-    private void addListener(){
-    	mic.setOnClickListener(this);
-    	b1.setOnClickListener(this);
-    	b2.setOnClickListener(this);
-    	
-    }
+		// Execute a transaction, replacing any existing fragment
+		// with this one inside the frame.
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.detail_view_fl, detailFrag);
+		ft.setTransition(FragmentTransaction.TRANSIT_ENTER_MASK);
+		ft.commit();
+	}
 
-    private int CaculateFrameLayoutValue(int percent) {
+	/**
+	 * @description 关闭详细页的Fragment，类本身调用（Activity 也要控制）
+	 * @param detailFrag
+	 */
+	public void ClosedDetailFragment(DetailFragment detailFrag) {
 
-    	return (int)(App.displayWidth * (percent/100f));
-    	
-    }
-    
-    public void setOffset(int xOffset, int yOffset) {
-    	secondMenuFrameLayout.scrollTo(xOffset, yOffset);
-    }
+		// Execute a transaction, replacing any existing fragment
+		// with this one inside the frame.
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.remove(detailFrag);
+		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+		ft.commit();
+	}
 
-    class GestureListener extends SimpleOnGestureListener {
-    	public boolean onScroll(MotionEvent e1, MotionEvent e2,  
-                float distanceX, float distanceY) {
-    		
-    		System.out.println();
-    		int length = CaculateFrameLayoutValue(10);
+	/**
+	 * @description 显示二级菜单
+	 * @param index topMenu 页面做菜单的所选 index，根据传入的不同时显示不同的 secondary菜单
+	 */
+	private void showSecondaryMenu(int index) {
+		secondaryMenuFrag = SecondaryMenuFragment.newInstance(index);
 
-    		float currentScrollDelta = e1.getX() - e2.getX();
-    		if(Math.abs(currentScrollDelta) < length) {
-    			int scrollOffset = Math.round(currentScrollDelta);
-    			
-    			setOffset(scrollOffset,0);
-    			
-    		}
+		// Execute a transaction, replacing any existing fragment
+		// with this one inside the frame.
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.replace(R.id.secondary_menu_view_fl, secondaryMenuFrag);
+		ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+		ft.commit();
+	}
 
-    		return true;
-    		
-    	}
-    }
-    
+	private void findView() {
+		mic = (ImageView) findViewById(R.id.mic);
+		b1 = (Button) findViewById(R.id.top_menu_button1);
+		b2 = (Button) findViewById(R.id.top_menu_button2);
+		secondMenuFrameLayout = (FrameLayout) findViewById(R.id.secondary_menu_view_fl);
+
+	}
+
+	private void addListener() {
+		mic.setOnClickListener(this);
+		b1.setOnClickListener(this);
+		b2.setOnClickListener(this);
+
+	}
+
+	/**
+	 * @description 根据当前设备的width 计算所需展示的 值 
+	 * @param percent 长度百分比
+	 * @return
+	 */
+	private int CaculateFrameLayoutValue(int percent) {
+
+		return (int) (App.displayWidth * (percent / 100f));
+
+	}
+
+	/**
+	 * @description 二级菜单所手指触摸滑动后， 执行返回相应位置的动画
+	 */
+	private void processScrollSnap() {
+		fixedAnimation.prepareAnimation();
+		secondMenuFrameLayout.startAnimation(fixedAnimation);
+	}
+
+
+	private void setOffset(int xOffset, int yOffset) {
+		secondMenuFrameLayout.scrollTo(xOffset, yOffset);
+	}
+
+	class GestureListener extends SimpleOnGestureListener {
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			if (e2.getX() > e1.getX()) {
+				isLeftPosition = false;
+			} else {
+				isLeftPosition = true;
+			}
+			return super.onFling(e1, e2, velocityX, velocityY);
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+
+			if (e2.getAction() == MotionEvent.ACTION_MOVE) {
+
+				if (e1.getX() >= fragmentMoveDistance && e1.getX() <= widthOf60) {
+
+					float currentScrollDelta = e1.getX() - e2.getX();
+					int scrollOffset = Math.round(currentScrollDelta);
+
+					setOffset(scrollOffset, 0);
+					isDrag = true;
+				}
+			}
+			return true;
+		}
+	}
+
+	class FixedPositionAnimation extends Animation {
+
+		private int mInitialOffset;
+		private int mTargetOffset;
+		private int mTargetDistance;
+
+		public void prepareAnimation() {
+
+			mInitialOffset = secondMenuFrameLayout.getScrollX();
+			mTargetOffset = 0;
+
+			if (isLeftPosition) {
+				mTargetDistance = mTargetOffset - mInitialOffset
+						+ CaculateFrameLayoutValue(10);
+			} else {
+				mTargetDistance = mTargetOffset - mInitialOffset;
+			}
+
+			this.setDuration(animationDuration);
+			this.setInterpolator(decelerateInterpolater);
+
+			isDrag = false;
+		}
+
+		@Override
+		protected void applyTransformation(float interpolatedTime,
+				Transformation transformation) {
+			interpolatedTime = (interpolatedTime > 1.0f) ? 1.0f
+					: interpolatedTime;
+			int offset = mInitialOffset
+					+ (int) (mTargetDistance * interpolatedTime);
+			setOffset(offset, 0);
+
+		}
+
+		@Override
+		public boolean getTransformation(long currentTime,
+				Transformation outTransformation) {
+
+			if (super.getTransformation(currentTime, outTransformation) == false) {
+				// 最后停在什么位置
+				if (isLeftPosition) {
+					setOffset(CaculateFrameLayoutValue(10), 0);
+				} else {
+					setOffset(0, 0);
+				}
+				return false;
+			}
+			return true;
+		}
+	}
 }
--- src/cn/zytec/lee/zyuiframe/SecondaryMenuFragment.java
+++ src/cn/zytec/lee/zyuiframe/SecondaryMenuFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class SecondaryMenuFragment extends Fragment implements OnClickListener {
